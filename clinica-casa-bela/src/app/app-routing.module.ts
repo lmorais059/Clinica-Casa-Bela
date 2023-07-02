a914bcb20eccb88a02c17ec30ebfc958b2f549a1
Lucas <lucasmoliveira059@gmail.com>
2023-07-02 05:08:11 -0300

    Estilizando páginas 1 e 2

--- clinica-casa-bela/src/app/app-routing.module.ts
+++ clinica-casa-bela/src/app/app-routing.module.ts
@@ -1,11 +1,22 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { Tab1Page } from './tab1/tab1.page';
 
 const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
-  }
+  },
+  {
+    path: 'tabs',
+    children: [
+      {
+        path: 'tab1',
+        component: Tab1Page
+      },
+      // outras rotas para as outras tabs...
+    ]
+  },
 ];
 @NgModule({
   imports: [
--- clinica-casa-bela/src/app/tab2/tab2.page.ts
+++ clinica-casa-bela/src/app/tab2/tab2.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-tab2',
@@ -7,6 +9,17 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
-  constructor() {}
+  constructor(private router: Router) {}
 
+login() {
+  // Lógica para autenticação e validação do formulário de login
+
+
+  // Redirecionar para a página da tab1
+  this.router.navigate(['/tabs/tab1']);
+
+  }
 }
+
+
+
